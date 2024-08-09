798d4a1411e399312d35cc5f5adda4b33f29acaf
duncmv <dtindiwensi@gmail.com>
2024-08-09 22:38:30 +0300

    cleanup testMail

--- tests/testMail.js
+++ tests/testMail.js
@@ -1,9 +1,5 @@
-const { configDotenv } = require('dotenv'); 
-configDotenv();
-const formData = require('form-data');
-const Mailgun = require('mailgun.js');
-const mailgun = new Mailgun(formData);
-const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
+import mg from '../config/emailService.js';
+
 //only works for specific emails because of a pricing issue  
 mg.messages.create('sandboxcc6abd444c3c47e3b0a57a7051ce7ead.mailgun.org', {
 	from: 'Block Events <mailgun@sandboxcc6abd444c3c47e3b0a57a7051ce7ead.mailgun.org>',
