dbff630d027c2445533d975484b25f8fd297d98f
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-08-10 23:41:39 +0300

    Update event controller to handle PUT requests and also the updated schema for events.

--- controllers/EventControllers.js
+++ controllers/EventControllers.js
@@ -1,62 +1,71 @@
 import Event from '../models/events';
 import User from '../models/user';
+import { validateLocation, validateId } from '../services/Validators';
 
 class EventController {
   // Current End Points are only for testing
   // DB connection will be handled with next steps
   static async createEvent(req, res) {
     const {
-      title, description, date, location, createdBy, attendees, tags,
+      title, description, startDateTime, endDateTime,
+      location, createdBy, attendees, tags, media,
+      isRecurring, recurrenceRule, catagory, status,
     } = req.body;
-    const dateDefault = Date.now();
+
     if (!title) { res.status(400).json({ error: 'Title is required' }); }
     if (!description) { res.status(400).json({ error: 'Description is required' }); }
-    if (!location) { res.status(400).json({ error: 'Location is required' }); }
+    if (!startDateTime) { res.status(400).json({ error: 'StartDateTime is required' }); }
     if (!createdBy) { res.status(400).json({ error: 'CreatedBy is required' }); }
+    if (!location && validateLocation(location)) { res.status(400).json({ error: 'Location is missing or not complete.' }); }
+    if (!tags) { res.status(400).json({ error: 'Tags is required' }); }
 
     try {
       const newEvent = new Event({
         title,
         description,
-        date: date || dateDefault,
         location,
+        startDateTime,
+        endDateTime,
         createdBy,
         attendees,
         tags,
+        media,
+        catagory,
+        status,
+        recurrenceRule,
+        isRecurring,
       });
 
       const event = await newEvent.save();
-      console.log(event);
-
+      // Update createdBy in User model
       await User.findByIdAndUpdate(
         createdBy,
         { $push: { createdEvents: event._id } },
         { new: true }, // Return the updated user
       );
-      res.json({ message: 'Event created successfully', newEvent });
+      res.status(201).json({ message: 'Event created successfully', newEvent });
     } catch (error) {
       // console.error('Error occurred while creating event:', error);
-      res.json({ message: 'Error occured while creating event', error });
+      res.status(500).json({ message: 'Error occured while creating event', error });
     }
   }
 
   static async getEventById(req, res) {
     const { id } = req.params;
-    if (!id) { res.status(400).json({ error: 'ID is required' }); }
 
-    const event = await Event.findOne({ _id: id }).catch((error) => {
-      res.json({ error: `Error occured while finding event: ${error.message} ` });
-    });
+    if (!id || validateId(id) === false) { return res.status(400).json({ error: 'Please provide appropriate Id' }); }
+
+    const event = await Event.findOne({ _id: id })
+      .catch((error) => (res.status(500).json({ error: `Error occured while finding event: ${error.message} ` })));
     if (!event) {
-      res.status(404).json({ error: 'Event not found' });
-    } else {
-      res.json(event);
+      return res.status(404).json({ error: 'Event not found' });
     }
+    return res.json(event);
   }
 
   static async getAllEvents(req, res) {
     const events = await Event.find().catch((error) => {
-      res.json({ error: `Error occured while finding event: ${error.message} ` });
+      res.status(500).json({ error: `Error occured while finding event: ${error.message} ` });
     });
     if (!events) {
       res.status(404).json({ error: 'Events not found' });
@@ -67,8 +76,26 @@ class EventController {
 
   static async updateEvent(req, res) {
     const { id } = req.params;
-    const { name, date } = req.query;
-    res.send(`Event with id: ${id} updated: date:${date} name: ${name} `);
+    if (!id || validateId(id) === false) { return res.status(400).json({ error: 'Please provide appropriate Id' }); }
+
+    try {
+      /* createdBy and attendees would need their own endpoints
+       to be updated for security and convenience
+       */
+      const notAllowed = ['createdBy', 'attendees', '_id'];
+      const reqBody = req.body;
+      const entries = Object.entries(reqBody);
+      const filteredEntries = entries.filter(([key]) => !notAllowed.includes(key));
+      const filteredObj = Object.fromEntries(filteredEntries);
+      const updatedEvent = await Event.findByIdAndUpdate(
+        id,
+        filteredObj,
+        { new: true },
+      );
+      return res.json({ message: 'Event updated.', updatedEvent });
+    } catch (error) {
+      return res.status(500).json({ message: 'Error occured while updating event', error });
+    }
   }
 
   static async deleteEvent(req, res) {
