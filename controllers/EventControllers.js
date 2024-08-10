e91e4e8fd7e3ec1c07916f71e79467bb41bec4ad
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-08-10 16:01:29 +0300

    Update event and user controllers to allow for new user or event and user creation, Update usercontroller for update of user data.

--- controllers/EventControllers.js
+++ controllers/EventControllers.js
@@ -1,24 +1,74 @@
 import Event from '../models/events';
+import User from '../models/user';
 
 class EventController {
   // Current End Points are only for testing
   // DB connection will be handled with next steps
   static async createEvent(req, res) {
-    res.send('Event created');
+    const {
+      title, description, date, location, createdBy, attendees, tags,
+    } = req.body;
+    const dateDefault = Date.now();
+    if (!title) { res.status(400).json({ error: 'Title is required' }); }
+    if (!description) { res.status(400).json({ error: 'Description is required' }); }
+    if (!location) { res.status(400).json({ error: 'Location is required' }); }
+    if (!createdBy) { res.status(400).json({ error: 'CreatedBy is required' }); }
+
+    try {
+      const newEvent = new Event({
+        title,
+        description,
+        date: date || dateDefault,
+        location,
+        createdBy,
+        attendees,
+        tags,
+      });
+
+      const event = await newEvent.save();
+      console.log(event);
+
+      await User.findByIdAndUpdate(
+        createdBy,
+        { $push: { createdEvents: event._id } },
+        { new: true }, // Return the updated user
+      );
+      res.json({ message: 'Event created successfully', newEvent });
+    } catch (error) {
+      // console.error('Error occurred while creating event:', error);
+      res.json({ message: 'Error occured while creating event', error });
+    }
   }
 
-  static async getEvent(req, res) {
+  static async getEventById(req, res) {
     const { id } = req.params;
-    if (id) {
-      res.send(`Event with ID: ${id} requested`);
+    if (!id) { res.status(400).json({ error: 'ID is required' }); }
+
+    const event = await Event.findOne({ _id: id }).catch((error) => {
+      res.json({ error: `Error occured while finding event: ${error.message} ` });
+    });
+    if (!event) {
+      res.status(404).json({ error: 'Event not found' });
+    } else {
+      res.json(event);
     }
-    res.send('All events requested');
+  }
+
+  static async getAllEvents(req, res) {
+    const events = await Event.find().catch((error) => {
+      res.json({ error: `Error occured while finding event: ${error.message} ` });
+    });
+    if (!events) {
+      res.status(404).json({ error: 'Events not found' });
+    } else {
+      res.json({ events });
+    }
   }
 
   static async updateEvent(req, res) {
     const { id } = req.params;
     const { name, date } = req.query;
-    res.send(`Event with id: ${id} updated: date :${date} name: ${name}`);
+    res.send(`Event with id: ${id} updated: date:${date} name: ${name} `);
   }
 
   static async deleteEvent(req, res) {
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,18 +4,70 @@ class UserController {
   // Current End Points are only for testing
   // DB connection will be handled with next steps
   static async createUser(req, res) {
-    const { userName, email, password } = req.query;
-    res.send(`User created with: ${userName}, ${email}, ${password}`);
+    const {
+      userName, email, password, profile,
+    } = req.body;
+    if (!email) { res.status(400).json({ error: 'Email is required' }); }
+    if (!password) { res.status(400).json({ error: 'Password is required' }); }
+
+    const newUser = new User({
+      userName: userName || email,
+      email,
+      password,
+      profile,
+    });
+
+    await newUser.save().then((user) => res.json(user)).catch((error) => {
+      res.json({ message: 'Error occured while saving user', error });
+    });
   }
 
-  static async getUser(req, res) {
+  static async getUserById(req, res) {
     const { id } = req.params;
-    res.send(`User with id: ${id} requested.`);
+    if (!id) { res.status(400).json({ error: 'ID is required' }); }
+
+    const user = await User.findOne({ _id: id }).catch((error) => {
+      res.json({ message: 'Error occured while getting user', error });
+    });
+
+    if (!user) {
+      res.status(404).json({ error: 'User not found' });
+    } else {
+      res.json({ user });
+    }
+  }
+
+  static async getAllUsers(req, res) {
+    const users = await User.find().catch((error) => {
+      res.json({ message: 'Error occured while getting users', error });
+    });
+    if (!users) {
+      res.status(404).json({ error: 'Users not found' });
+    } else {
+      res.json({ users });
+    }
   }
 
   static async updateUser(req, res) {
-    const { userName, email, password } = req.query;
-    res.json({ message: `User updated with: ${userName}, ${email}, ${password}` });
+    const { id } = req.params;
+    if (!id) { res.status(400).json({ error: 'ID is required' }); }
+
+    const user = await User.findOne({ _id: id });
+    if (!user) { res.status(404).json({ error: 'User not found' }); }
+
+    // password and email would need their own handler to update for security reasons
+    const notAllowed = ['password', 'email'];
+    const reqBody = req.body;
+    const entries = Object.entries(reqBody);
+    const filteredEntries = entries.filter(([key]) => !notAllowed.includes(key));
+    const filteredObj = Object.fromEntries(filteredEntries);
+    console.log(filteredObj);
+    const updatedInfo = await User.findByIdAndUpdate(
+      id,
+      filteredObj,
+      { new: true }, // Return the updated user
+    );
+    res.json({ message: 'User information updated', updatedInfo });
   }
 
   // Only for admin
