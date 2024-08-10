703663d9669268ebe867542afb1cd926cee2b556
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-08-11 02:55:10 +0300

    Add new routes for user and event controllers.

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,17 +1,25 @@
 import express from 'express';
 import EventController from '../controllers/EventControllers';
+import AuthUser from '../middleware/authToken';
 
 const router = express.Router();
 
-// Get all events
-router.get('/', EventController.getAllEvents);
+// GET all, GET by id, POST, PUT, DELETE events
+router.get('/', AuthUser, EventController.getAllEvents);
+router.get('/:id', AuthUser, EventController.getEventById);
+router.post('/', AuthUser, EventController.createEvent);
+router.put('/:id', AuthUser, EventController.updateEvent);
+router.delete('/:id', AuthUser, EventController.deleteEvent);
 
-router.get('/:id', EventController.getEventById);
+// Register and Unregister for an event
+router.put('/:id/register', AuthUser, EventController.registerEvent);
+router.put('/:id/unregister', AuthUser, EventController.unregisterEvent);
 
-router.post('/', EventController.createEvent);
-
-router.put('/:id', EventController.updateEvent);
-
-router.delete('/:id', EventController.deleteEvent);
+// Get events by creator
+router.get('/creator/:id', AuthUser, EventController.getEventsByCreator);
+// Get events by user
+router.get('/user/:id/userevents', AuthUser, EventController.getRegisteredEvents);
+// Get event attendees
+router.get('/event/:id/attendees', AuthUser, EventController.getEventAttendees);
 
 export default router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,16 +4,10 @@ import AuthUser from '../middleware/authToken';
 
 const router = express.Router();
 
-// Define user routes
 router.post('/', AuthUser, UserController.createUser);
-
 router.get('/:id', AuthUser, UserController.getUserById);
-
 router.get('/', AuthUser, UserController.getAllUsers);
-
 router.put('/:id', AuthUser, UserController.updateUser);
-
-// May not be needed as of now: Only for admin
 router.delete('/:id', AuthUser, UserController.deleteUser);
 
 export default router;
