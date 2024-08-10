bbd090456db8ec5ea1c51d5be2cd173c4f60ff92
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-08-10 16:03:30 +0300

    update user model to abide camelCase naming,

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-  username: {
+  userName: {
     type: String,
     required: false,
     unique: true,
