f257d35b1316a4cc01a998de3eac5a240084dfb0
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-08-17 23:57:52 +0300

    Update routes for auth with new route to check status of authentication

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
+router.get('/check', AuthController.check);
 router.post('/logout', AuthController.logout);
 router.get('/users', AuthController.protect, AuthController.restrict('admin'), AuthController.getAllUsers);
 
