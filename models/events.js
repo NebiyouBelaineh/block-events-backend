9460d91c59d43b36ebf19e84f4030b3516fce304
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-08-21 02:23:40 +0300

    Remove extra field attendees

--- models/events.js
+++ models/events.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const { Schema } = mongoose;
-
 const eventSchema = new mongoose.Schema({
   title: { type: String },
   description: { type: String },
@@ -18,10 +16,6 @@ const eventSchema = new mongoose.Schema({
   tags: [{ type: String }],
   media: { type: String }, // For storing media file URLs or identifiers
   category: { type: String },
-  attendees: [{
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  }],
   status: { type: String, enum: ['active', 'inactive'], default: 'active' },
 });
 
@@ -32,71 +26,3 @@ eventSchema.index({ startDateTime: 1 });
 const Event = mongoose.model('Event', eventSchema);
 
 module.exports = Event;
-
-// import mongoose from 'mongoose';
-
-// const { Schema, model } = mongoose;
-
-// const eventSchema = new Schema({
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   description: {
-//     type: String,
-//     required: true,
-//   },
-//   startDateTime: {
-//     type: Date,
-//     required: true,
-//   },
-//   endDateTime: {
-//     type: Date,
-//   },
-//   location: {
-//     type: String,
-//     required: false,
-//   },
-//   createdBy: {
-//     type: Schema.Types.ObjectId,
-//     ref: 'User',
-//     required: true,
-//   },
-//   organizer: {
-//     name: {
-//       type: String,
-//       required: false,
-//     },
-//     email: {
-//       type: String,
-//       required: false,
-//     },
-//     phone: {
-//       type: String,
-//       required: false,
-//     },
-//   },
-//   attendees: [{
-//     type: Schema.Types.ObjectId,
-//     ref: 'User',
-//   }],
-//   tags: {
-//     type: [String],
-//     required: true,
-//   },
-//   media: {
-//     pictures: [{ type: String }],
-//     videos: [{ type: String }],
-//   },
-//   status: {
-//     type: String,
-//     enum: ['Scheduled', 'Cancelled', 'Completed'],
-//     default: 'Scheduled',
-//   },
-//   category: {
-//     type: String,
-//   },
-// });
-
-// const Event = model('Event', eventSchema);
-// export default Event;
