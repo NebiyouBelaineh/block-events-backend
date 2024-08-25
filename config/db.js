20ffdd216b2353b53b55e80ed0fb1953160de9d4
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-08-25 18:43:17 +0300

    Use dbconnection string

--- config/db.js
+++ config/db.js
@@ -13,14 +13,19 @@ const localDBUrl = process.env.DB_CLUSTER_LOCAL_URL;
 
 // eslint-disable-next-line no-unused-vars
 const dbUri = `mongodb+srv://${username}:${password}${cluster}`;
+// eslint-disable-next-line no-unused-vars
+const dbConnectionString = process.env.DB_CONNECTION_STRING;
+// console.log(dbConnectionString);
 
 const connectDB = async () => {
   try {
     /* Connect to MongoDB atlas */
+    await mongoose.connect(dbConnectionString);
     // await mongoose.connect(dbUri);
 
     /* Connect to local MongoDB */
-    await mongoose.connect(localDBUrl);
+    // await mongoose.connect(localDBUrl);
+
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
